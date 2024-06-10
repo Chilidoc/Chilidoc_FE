1d141440b3b1bbff2e5dbe3cf6d5215739b82db2
MhdFikri44 <mhdfikr44@gmail.com>
2024-06-10 20:06:38 +0700

    initial commit

--- app/src/main/java/com/capstone/chilidoc/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/chilidoc/ui/register/RegisterActivity.kt
@@ -1,21 +1,63 @@
 package com.capstone.chilidoc.ui.register
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.chilidoc.R
+import com.capstone.chilidoc.databinding.ActivityRegisterBinding
+import com.capstone.chilidoc.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loginPage()
+    }
+
+    private fun loginPage() {
+        val text = "Already have an account? Login"
+        val spannableString = SpannableString(text)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
+
+        val foregroundColorSpan = ForegroundColorSpan(Color.parseColor("#0000FF"))
+
+        val startIndex = text.indexOf("Login")
+        val endIndex = startIndex + "Login".length
+
+        spannableString.setSpan(
+            clickableSpan,
+            startIndex,
+            endIndex,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            foregroundColorSpan,
+            startIndex,
+            endIndex,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.tvHaveAccount.text = spannableString
+        binding.tvHaveAccount.movementMethod = LinkMovementMethod.getInstance()
     }
 }
